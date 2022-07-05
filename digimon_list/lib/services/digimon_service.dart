a797bcae423b9868034b8b00924aaa641988bdad
wilker123 <josewilkerlucena123@gmail.com>
2022-07-05 15:04:55 -0300

    Create method get Digimons in Digimon Service class

---
+++ digimon_list/lib/services/digimon_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:digimon_list/models/digimon.dart';
+import 'package:http/http.dart' as http;
+
+class DigimonService {
+  Future<List<Digimon>?> getDigimons() async {
+    final response = await http
+        .get(Uri.https("https://digimon-api.herokuapp.com/", "/api/digimon"));
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((e) => Digimon.fromJson(e)).toList();
+    }
+
+    return null;
+  }
+}
